0bcdb07a44edd494a38462423aed3ef7c18cbb2a
Ezeh20 <ranter1570@gmail.com>
2023-09-26 03:21:30 +0100

    cleaning up

--- src/components/ComingSoon/constants.js
+++ src/components/ComingSoon/constants.js
@@ -1,4 +1,4 @@
 export const constants = {
     p1: `This feature is currently unavailable`,
-    p2: `you can use the locate me feature for a Pinpoint result.`
+    p2: `you can use the locate me feature for a Pinpoint result. Please you need to Allow access`
 }
